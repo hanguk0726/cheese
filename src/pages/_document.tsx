94147eb0c4b8887fcdf897e1f7ff9709a03deb2a
hanguk0726 <hanguk0726@gmail.com>
2025-01-10 20:14:36 +0900

    font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="emotion-insertion-point" content="" />
+        <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css'></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
